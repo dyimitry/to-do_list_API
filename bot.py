e6cd6ebe8efecee23f57c9857304bbcffd2c5744
Дмитрий Шинкарев <dimonshinkariov111@yandex.ru>
2023-10-25 00:59:33 +0300

    user complete.done

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def start(message):
     # }
     result = clientDB.registration(us_id, us_name, us_sname, username)
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
     btn1 = types.InlineKeyboardButton(text="Создать новую задачу", callback_data="create_task")
     btn2 = types.InlineKeyboardButton(text="Показать мои задачи", callback_data="get_tasks")
     markup.add(btn1, btn2)
@@ -66,11 +66,12 @@ def start(message):
         bot.send_message(
             message.chat.id,
             text="Напишите свою задачу в формате:\n"
-                 "название задачи: ???\n"
-                 "описание задачи: ???")
+                 "Создать задачу\n"
+                 "Название задачи: «Необходимо написать»\n"
+                 "Описание задачи: «Необходимо написать»")
 
     if message.text == "Показать мои задачи":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
         btn1 = types.InlineKeyboardButton(text="Изменить задачу", callback_data="update_task")
         btn2 = types.InlineKeyboardButton(text="Удалить задачу", callback_data="delete_tasks")
         markup.add(btn1, btn2)
@@ -78,7 +79,7 @@ def start(message):
         result = clientDB.get_tasks(user_id)
         stroka = ''
         for i in result:
-            stroka += f"{i['name']}: {i['description']}, {i['status']}, {i['id']}\n"
+            stroka += f"Задача: №{i['id']}, Название задачи:{i['name']}, Описание задачи:{i['description']}, Статус:{i['status']}\n"
         bot.send_message(
             message.chat.id,
             text=stroka,
@@ -87,9 +88,9 @@ def start(message):
     if message.text == "Удалить задачу":
         bot.send_message(
             message.chat.id,
-            text="Напишите номер задачи которую необходимо удалить")
+            text="Напишите номер задачи которую необходимо удалить (числом)")
     # Удалить задачу написав в тг ее id
-    if message.text in list(map(str,range(1000))):
+    if message.text.isdigit():
         result = clientDB.delete_task(message.text)
         if len(result) == 1:
             stroka = result['detail']
@@ -97,38 +98,63 @@ def start(message):
                 message.chat.id,
                 text=stroka)
             return
-        stroka = f"Задача: №{result['id']}. name:{result['name']} успешно удалена"
+        stroka = (f"Задача: №{result['id']}. Название задачи:{result['name']}.\n"
+                  f"Успешно удалена")
         bot.send_message(
             message.chat.id,
             text=stroka)
     if message.text == "Изменить задачу":
         bot.send_message(
             message.chat.id,
-            text="Напишите название задачи которую необходимо изменить:\n"
-                 " задачу:№_\n"
-                 "название:???\n"
-                 "описание:???")
-    # if message.text.split(':')[0] == "задачу":
-    #     result = clientDB.update_task()
-    #     bot.send_message(
-    #         message.chat.id,
-    #         text="Напишите название задачи которую необходимо изменить: задачу № _")
-    else:
-        # СОЗДАТЬ ЗАДАЧУ
+            text="Если Вы хотите изменить статус задачи,\n"
+                 "Необходимо передать новые данные в формате:\n"
+                 "Задача №:«Необходимо написать»\n"
+                 "Статус: «Необходимо написать»")
+    # ИЗМЕНИТЬ ЗАДАЧУ
+    if message.text.startswith("Задача №:"):
         message_lines = message.text.split("\n")
-        if len(message_lines) == 2:
-            task_name = message_lines[0].split(":")[1]
-            task_descripton = message_lines[1].split(":")[1]
+
+        number = message_lines[0].split(":")[1].strip()
+        if number.isdigit():
+            status = message_lines[1].split(":")[1].strip()
+            stat = None
+
+            if status == "True":
+                stat = True
+            if stat is None:
+                bot.send_message(
+                    message.chat.id,
+                    text='Если задача выполнена то нужно изменить статус на True без отспупа от «:»')
+                return
+            result_get_task = clientDB.get_task(number)
+            if result_get_task.get('detail'):
+                bot.send_message(
+                    message.chat.id,
+                    text=f"{result_get_task['detail']}")
+                return
+            name = result_get_task['name']
+            description = result_get_task['description']
+            result = clientDB.update_task(number, name, description, stat)
+            bot.send_message(
+                message.chat.id,
+                text="Статус задачи успешно изменен")
+            return
+        bot.send_message(
+            message.chat.id,
+            text="Необходимо написать № числом")
+    # СОЗДАТЬ ЗАДАЧУ
+    if message.text.startswith("Создать задачу"):
+        message_lines = message.text.split("\n")
+        try:
+            task_name = message_lines[1].split(":")[1]
+            task_descripton = message_lines[2].split(":")[1]
             user_id = message.from_user.id
             status = False
             result = clientDB.create_task(task_name, task_descripton, status, user_id)
-
-
-# @bot.callback_query_handler(func=lambda call: call.data == "delete_tasks")
-# def button_pressed_handler(call):
-#     bot.send_message(
-#         call.message.chat.id,
-#         text="Напишите номер задачи")
+        except:
+            bot.send_message(
+                message.chat.id,
+                text='Необходимо установить «:»')
 
 
 bot.polling(none_stop=True)
