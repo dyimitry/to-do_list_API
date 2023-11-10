f88cdacc7c02fce36ef9839a41685ec161156c8f
Дмитрий Шинкарев <dimonshinkariov111@yandex.ru>
2023-11-11 01:27:42 +0300

    producer and worker complete.done

--- notification/engine.py
+++ notification/engine.py
@@ -1,15 +1,24 @@
+import logging
+from datetime import timedelta
+
 from celery import Celery
 
 from app.core.config import settings
 
+# Configure the logging for Celery Beat
+beat_logger = logging.getLogger('celery.beat')
+beat_logger.setLevel(logging.DEBUG)
+beat_logger.addHandler(logging.StreamHandler())
 
 app = Celery("notification", broker=settings.CELERY_BROKER_URL, backend='rpc://', include=['notification.tasks'])
+
 
 app.conf.beat_schedule = {
     'refresh': {
         'task': 'producer',
-        'schedule': 5.0
+        'schedule': timedelta(seconds=5),
+        'options': {
+            'queue': 'notification',
+        }
     }
 }
-
-
